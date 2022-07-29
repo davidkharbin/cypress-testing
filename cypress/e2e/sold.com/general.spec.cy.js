d2c640668ffa3a53dc3ad0a55e1f711831d5e4f9
davidkharbin <david_k_harbin@hotmail.com>
2022-07-29 14:13:02 -0600

    test(general): stubbs 7 tests

--- cypress/e2e/sold.com/general.spec.cy.js
+++ cypress/e2e/sold.com/general.spec.cy.js
@@ -1,12 +1,22 @@
 describe('react-todomvc', () => {
-
   beforeEach(() => {
     cy.visit('http://0.0.0.0:8000')
   })
-
 
   it('Main and footer sections don\'t exist upon initial page display', () => {
     cy.get('.footer').should('not.exist')
     cy.get('.main').should('not.exist')
   })
+
+  it('On page load, focus is on the todo input field'), () => { }
+
+  it('Adding a todo clears the text input field'), () => { }
+
+  it('Main and footer sectoins show after adding a todo'), () => { }
+
+  it('Adds a todo'), () => { }
+
+  it('Addds three todos'), () => { }
+
+  it('Trims todo whitespace'), () => { }
 })
